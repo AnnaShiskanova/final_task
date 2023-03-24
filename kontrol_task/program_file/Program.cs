e1a04dd605f4a42796120ac946318cc5f5a4dd26
Anna <shiskanovaag@mail.ru>
2023-03-24 20:15:39 +1000

    clear

--- kontrol_task/program_file/Program.cs
+++ kontrol_task/program_file/Program.cs
@@ -6,36 +6,28 @@
     {
         Console.WriteLine($"Введите {i+1}-й элемент: ");
         string element = Convert.ToString(Console.ReadLine());
-        arr[i] = element;
-        
+        arr[i] = element; 
     }
-    foreach (var item in arr) 
-        Console.Write($"{item}, ");
+    foreach (var el in arr) 
+        Console.Write($"{el}, ");
     System.Console.WriteLine();
 
     string[] resultArray = new string[arr.Length];
+    
     void ResultArray(string[] arr, string[] resultArray)
     {
         int j = 0;
-        // foreach (var el in arr)
-        // {
-        //     if (el.Length <= max)
-        //     Console.Write($"{el}, ");
-        // }
-       
+        int max = 3; 
         for (int i = 0; i < arr.Length; i++)
         {
-            if (arr[i].Length <= 3)
+            if (arr[i].Length <= max)
                 {
                    resultArray[j] = arr[i];
                     j++;
                 }
-
-            
         }
-
-        
     }
+
     ResultArray(arr, resultArray);
    
    PrintArray(resultArray);
